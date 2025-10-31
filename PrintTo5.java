1d3f0d394f76b1fcfa8b6313eeacb9cd6d5b1e91
elayhakim <elay.hakim@post.runi.ac.il>
2025-10-31 12:47:50 +0200

    PrintTo5

--- PrintTo5.java
+++ PrintTo5.java
@@ -3,7 +3,12 @@
 public class PrintTo5 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
-        int i = 0;
         // Complete the program's code here:
+                int i = 0;
+        while (i<6) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Done");
     }
  }
